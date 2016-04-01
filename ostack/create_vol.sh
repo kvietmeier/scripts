f4a31ad701d4003e769121b58de6cf13f6738a8d
Karl Vietmeier <karlv@storagenet.org>
2016-04-01 13:29:36 -0700

    Update Files

--- ostack/create_vol.sh
+++ ostack/create_vol.sh
@@ -2,34 +2,34 @@
 # Create Nova instances based on existing volumes of type "XtremIO"
 
 
-echo ""
-echo "====================================================="
-echo "There are Currently no Volumes Created in the Project"
-echo "====================================================="
-echo ""
+echo "`date` = "
+echo "`date` = ====================================================="
+echo "`date` = There are Currently no Volumes Created in the Project"
+echo "`date` = ====================================================="
+echo "`date` = "
 cinder list
-echo ""
-echo "`date` - Creating 500 Volumes"
-echo ""
+echo "`date` = "
+echo "`date` = Creating 500 Volumes"
+echo "`date` = "
 
 # wait for input
-echo "Press <RETURN> to continue"
+echo "`date` = Press <RETURN> to continue"
 read 
 
 for vol in {001..20}
    do
-     echo "`date` : cinder create 10 --display-name XtremIO_Vol${vol} --volume-type XtremIO"
+     echo "`date` = cinder create 10 --display-name XtremIO_Vol${vol} --volume-type XtremIO"
      #echo "...."
      #cinder create 10 --display-name XtremIO_Vol${vol} --volume-type XtremIO &> /dev/null
    done
 sleep 1
-echo ""
-echo ""
-echo "`date` - Completed Volume Creation"
+echo "`date` = "
+echo "`date` = "
+echo "`date` = Completed Volume Creation"
 sleep 1
-echo "`date` - cinder list"
+echo "`date` = cinder list"
 cinder list
-echo ""
-echo "`date` - $(cinder list | grep Xtr | wc -l) Volumes Created" 
-echo ""
+echo "`date` = "
+echo "`date` = $(cinder list | grep Xtr | wc -l) Volumes Created" 
+echo "`date` = "
 sleep 5
