d4ed93e670dd9123fcd98499396dc4c83f374e18
Karl Vietmeier <9323750+kvietmeier@users.noreply.github.com>
2018-04-12 15:03:51 -0700

    Added PS function

---
+++ powershell/Functions.ps1
@@ -0,0 +1,24 @@
+# Am I on my corp network and need proxies?
+# Use Get-DnsClientGlobalSetting
+# https://docs.microsoft.com/en-us/powershell/module/dnsclient/get-dnsclientglobalsetting?view=win10-ps
+# https://docs.microsoft.com/en-us/powershell/module/dnsclient/get-dnsclient?view=win10-ps
+
+# Domain to match
+$dnsDomain = "intel"
+
+function CheckCorpDomain ($domain) 
+{
+    # Parse out FQDN
+    $dnsDomainList = Get-DnsClientGlobalSetting
+    $FQDN = $dnsDomainList.SuffixSearchList | Select-Object -First 1
+
+    # create an array
+    $dnsParts = $FQDN.Substring($FQDN.IndexOf(".") + 1).split(".")
+
+    # Find the element that matches
+    $corpDomain = $dnsParts -contains $domain
+    return $corpDomain
+}
+
+$needProxy = CheckCorpDomain ($dnsDomain)
+Write-Host $needProxy
