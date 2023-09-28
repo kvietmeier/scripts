c9441f26a23d5616ccc3600f3419a7eb59408e8c
kvietmeier <karlv@storagenet.org>
2023-09-28 21:16:32 +0000

    update scrtipts

---
+++ get_cpu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# Get CPU type from hosts
+
+for i in {2..5}
+do  	
+  cpu=$(ssh ubuntu@vdb-0$i "cat /proc/cpuinfo | sed -n -E '/^model\s*(name|)\s*:/{s/.*:\s*//;p}' | head -n2 | tr '\n\n' ':'")
+  echo "vdb-0$i:  $cpu"
+done
