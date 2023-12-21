f8d30eada8929e051fba538851a6ec172c9b1c35
kvietmeier <karlv@storagenet.org>
2023-12-21 15:18:25 -0800

    minor fixes

--- dstat_azure_nvme.sh
+++ dstat_azure_nvme.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/bash -x
+#!/usr/bin/bash
 ###==========================================================================================================###
 #
-#    Basic Usage: Run dool and collect output into csv files.
 #    Basic Usage: Run dool and collect output into csv files.
 #    Collect system profile while monitoring disk usage during benchmark runs or performance troubleshooting
 #    Created By:   Karl Vietmeier
@@ -9,31 +8,13 @@
 #
 #    Written for an Azure Ubuntu VM with NVME controllers
 #
-###==========================================================================================================### 
 ###==========================================================================================================###
 
-#-- Variables/Setup
+###-- Variables/Setup
 
 # Enumerate the nvme drives
 nvme_drives=($(ls -l /dev/disk/by-path | grep nvme | grep -v part | awk '{print substr($11, 7)}' | sort))
 disks=$(IFS=,; echo "${nvme_drives[*]}")
-
-# Setup directories/output files
-CURRENT_TIME=$(date +%m%d:%H%M)
-OUTPUTDIR=${HOME}/dool/
-OUTPUTDIR=${HOME}/dool/
-
-# Check if the directory for output exists
-if [ ! -d $OUTPUTDIR ] ; then
-  mkdir $OUTPUTDIR 2> /dev/null
-fi
-
-OUTPUTFILE_proc=${OUTPUTDIR}dool_process.${CURRENT_TIME}.csv
-OUTPUTFILE_io=${OUTPUTDIR}dool_io.${CURRENT_TIME}.csv
-OUTPUTFILE_sys=${OUTPUTDIR}dool_sys.${CURRENT_TIME}.csv
-OUTPUTFILE_proc=${OUTPUTDIR}dool_process.${CURRENT_TIME}.csv
-OUTPUTFILE_io=${OUTPUTDIR}dool_io.${CURRENT_TIME}.csv
-OUTPUTFILE_sys=${OUTPUTDIR}dool_sys.${CURRENT_TIME}.csv
 
 # Command intervals (s)short, (m)edium, (l)ong, (ll)onger
 sinterval=1
@@ -49,6 +30,21 @@ dool_io_flags="-D total,$disks -N eth0"
 dool_proc_flags="--time -p -c --disk --mem --top-cpu --top-bio --top-latency "
 dool_sys_flags="--time --load --proc --cpu --sys --vm --disk  --net -N eth0"
 
+# Setup directories/output files
+CURRENT_TIME=$(date +%m%d:%H%M)
+OUTPUTDIR=${HOME}/dool/
+
+# Check if the directory for output exists
+if [ ! -d $OUTPUTDIR ] ; then
+  mkdir $OUTPUTDIR 2> /dev/null
+fi
+
+OUTPUTFILE_proc=${OUTPUTDIR}dool_process.${CURRENT_TIME}.csv
+OUTPUTFILE_io=${OUTPUTDIR}dool_io.${CURRENT_TIME}.csv
+OUTPUTFILE_sys=${OUTPUTDIR}dool_sys.${CURRENT_TIME}.csv
+
+### Run dool
+# dool command line
 dool_io="dool $dool_io_flags --output $OUTPUTFILE_io $linterval $count"
 dool_process="dool $dool_proc_flags --output $OUTPUTFILE_proc $linterval $count"
 dool_sys="dool $dool_sys_flags --output $OUTPUTFILE_sys $linterval $count"
@@ -56,25 +52,17 @@ dool_sys="dool $dool_sys_flags --output $OUTPUTFILE_sys $linterval $count"
 
 ###-- Functions
 # Run cmd in background
-bg_cmd()
-  {
-    cmd="$@"
-    ${cmd} &>/dev/null &
-    disown
-  }
+bg_cmd() {
+  cmd="$@"
+  ${cmd} &>/dev/null &
+  disown
+}
 
 #-- End Functions
 
-
 ###-- Main 
-#bg_cmd ${dool_process}
-#bg_cmd ${dool_disk}
-#bg_cmd ${dool_sys}
-
-
-dool $dool_proc_flags --output $OUTPUTFILE_proc $sinterval $count
-
-
-
+bg_cmd ${dool_process}
+bg_cmd ${dool_io}
+bg_cmd ${dool_sys}
 
 
