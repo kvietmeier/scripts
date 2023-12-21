f638c3982bee689f49e4e9313f7cfa58907d856e
kvietmeier <karlv@storagenet.org>
2023-12-21 15:10:27 -0800

    fixed disk IO

--- dstat_azure_nvme.sh
+++ dstat_azure_nvme.sh
@@ -1,25 +1,14 @@
-#!/usr/bin/bash
-###==========================================================================================================### 
+#!/usr/bin/bash -x
+###==========================================================================================================###
 #
-#    Basic Usage: Run dstat and collect output into csv files.
+#    Basic Usage: Run dool and collect output into csv files.
 #    Collect system profile while monitoring disk usage during benchmark runs or performance troubleshooting
 #    Created By:   Karl Vietmeier
 #                  Cloud Solutions Architect, Intel
-#  
+#
 #    Written for an Azure Ubuntu VM with NVME controllers
 #
-###==========================================================================================================### 
-
-#-- Functions
-# Run cmd in background
-bg_cmd()
-  {
-    cmd="$@"
-    ${cmd} &>/dev/null &
-    disown
-  }
-
-#-- End Functions
+###==========================================================================================================###
 
 #-- Variables/Setup
 
@@ -29,36 +18,49 @@ disks=$(IFS=,; echo "${nvme_drives[*]}")
 
 # Setup directories/output files
 CURRENT_TIME=$(date +%m%d:%H%M)
-OUTPUTDIR=${HOME}/dstat/
+OUTPUTDIR=${HOME}/dool/
 
 # Check if the directory for output exists
 if [ ! -d $OUTPUTDIR ] ; then
   mkdir $OUTPUTDIR 2> /dev/null
 fi
 
-OUTPUTFILE_proc=${OUTPUTDIR}dstat_process.${CURRENT_TIME}.csv
-OUTPUTFILE_io=${OUTPUTDIR}dstat_io.${CURRENT_TIME}.csv
-OUTPUTFILE_sys=${OUTPUTDIR}dstat_sys.${CURRENT_TIME}.csv
+OUTPUTFILE_proc=${OUTPUTDIR}dool_process.${CURRENT_TIME}.csv
+OUTPUTFILE_io=${OUTPUTDIR}dool_io.${CURRENT_TIME}.csv
+OUTPUTFILE_sys=${OUTPUTDIR}dool_sys.${CURRENT_TIME}.csv
 
-# Command intervals
+# Command intervals (s)short, (m)edium, (l)ong, (ll)onger
 sinterval=1
 minterval=10
 linterval=30
 llinterval=3600
+
+# Duration
 count=380
 
-# dstat flags
-dstat_io_flags="-D total,$disks -N eth0"
-dstat_proc_flags="--time -p -c --disk --mem --top-cpu --top-bio --top-latency "
-dstat_sys_flags="--time --load --proc --cpu --sys --vm --disk  --net -N eth0"
+# dool flags
+dool_io_flags="-D total,$disks -N eth0"
+dool_proc_flags="--time -p -c --disk --mem --top-cpu --top-bio --top-latency "
+dool_sys_flags="--time --load --proc --cpu --sys --vm --disk  --net -N eth0"
 
-dstat_io="dstat $dstat_io_flags --output $OUTPUTFILE_io $linterval $count"
-dstat_process="dstat $dstat_proc_flags --output $OUTPUTFILE_proc $linterval $count"
-dstat_sys="dstat $dstat_sys_flags --output $OUTPUTFILE_sys $linterval $count"
+dool_io="dool $dool_io_flags --output $OUTPUTFILE_io $minterval $count"
+dool_process="dool $dool_proc_flags --output $OUTPUTFILE_proc $minterval $count"
+dool_sys="dool $dool_sys_flags --output $OUTPUTFILE_sys $minterval $count"
 
 
-#-- Main 
-bg_cmd ${dstat_process}
-bg_cmd ${dstat_disk}
-bg_cmd ${dstat_sys}
+###-- Functions
+# Run cmd in background
+function bg_cmd ()
+  {
+    cmd="$@"
+    ${cmd} &>/dev/null &
+    disown
+  }
 
+#-- End Functions
+
+
+###-- Main - put the comands into the background
+bg_cmd ${dool_process}
+bg_cmd ${dool_sys}
+bg_cmd ${dool_io}
