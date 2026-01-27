b17f2ee9cee95aba507151508a8d9d355329d227
kvietmeier <karlv@storagenet.org>
2026-01-27 10:28:56 -0800

    new instructions

--- gcp/create_vpn_2_azure-commands.sh
+++ gcp/create_vpn_2_azure-commands.sh
@@ -4,21 +4,30 @@ GCP_REGION="us-central1"
 GCP_VPC="karlv-corevpc"
 HA_VPN_GW_NAME="vpn-gateway-azure-central1"
 ROUTER_NAME="router-azure-central1"
-EXTERNAL_GW_NAME="vpngw-azure"
+EXTERNAL_GW_NAME="vpn-gateway-azure-central1"
 GCP_ASN="65333"
 TUNNEL_IF0="vpn-tunnel-azure-central1-if0"
 TUNNEL_IF1="vpn-tunnel-azure-central1-if1"
 INTERFACE0="azure-tunnel-if0"
 INTERFACE1="azure-tunnel-if1"
+GCP_APIPA_BGP_A="169.254.21.9"
+GCP_APIPA_BGP_B="169.254.22.9"
 BGP_PEER_IF0="azure-bgp-peer-if0"
 BGP_PEER_IF1="azure-bgp-peer-if1"
 PRIORITY=100
-SHARED_KEY='YourStrong!Passw0rd'
-AZURE_PUBIP0="20.121.130.26"
-AZURE_PUBIP1="172.172.233.46"
-AZURE_APIPA_BGP_A="169.254.21.2"
-AZURE_APIPA_BGP_B="169.254.22.2"
-AZURE_ASN_B="65006"
+SHARED_KEY='xVTsD61QPvDUPgD3bJvyaxo6s+peCTD6'
+AZURE_PUBIP0="4.249.107.59"
+AZURE_PUBIP1="4.249.107.48"
+AZURE_APIPA_BGP_A="169.254.21.10"
+AZURE_APIPA_BGP_B="169.254.22.10"
+AZURE_ASN_B="65010"
+
+# Generate a new key
+openssl rand -base64 24
+xVTsD61QPvDUPgD3bJvyaxo6s+peCTD6
+
+
+
 
 # =========================================================================
 # --- COMMANDS TO BUILD THE HA VPN CONNECTION ---
@@ -34,6 +43,7 @@ gcloud compute routers create "$ROUTER_NAME" --network "$GCP_VPC" --region "$GCP
 # 3. Create the External VPN Gateway (Azure Side)
 gcloud compute external-vpn-gateways create "$EXTERNAL_GW_NAME" --interfaces 0="$AZURE_PUBIP0",1="$AZURE_PUBIP1"
 
+### - To add new connection - start here
 # 4. Create VPN Tunnel 0
 gcloud compute vpn-tunnels create "$TUNNEL_IF0" --peer-external-gateway="$EXTERNAL_GW_NAME" --peer-external-gateway-interface=0 --region="$GCP_REGION" --ike-version=2 --shared-secret="$SHARED_KEY" --router="$ROUTER_NAME" --vpn-gateway="$HA_VPN_GW_NAME" --interface=0
 
@@ -41,10 +51,10 @@ gcloud compute vpn-tunnels create "$TUNNEL_IF0" --peer-external-gateway="$EXTERN
 gcloud compute vpn-tunnels create "$TUNNEL_IF1" --peer-external-gateway="$EXTERNAL_GW_NAME" --peer-external-gateway-interface=1 --region="$GCP_REGION" --ike-version=2 --shared-secret="$SHARED_KEY" --router="$ROUTER_NAME" --vpn-gateway="$HA_VPN_GW_NAME" --interface=1
 
 # 6. Add Router Interface 0 (connects tunnel 0 to router)
-gcloud compute routers add-interface "$ROUTER_NAME" --interface-name="$INTERFACE0" --vpn-tunnel="$TUNNEL_IF0" --region="$GCP_REGION"
+gcloud compute routers add-interface "$ROUTER_NAME" --interface-name="$INTERFACE0" --vpn-tunnel="$TUNNEL_IF0" --ip-address="$GCP_APIPA_BGP_A" --mask-length=30 --region="$GCP_REGION"
 
 # 7. Add Router Interface 1 (connects tunnel 1 to router)
-gcloud compute routers add-interface "$ROUTER_NAME" --interface-name="$INTERFACE1" --vpn-tunnel="$TUNNEL_IF1" --region="$GCP_REGION"
+gcloud compute routers add-interface "$ROUTER_NAME" --interface-name="$INTERFACE1" --vpn-tunnel="$TUNNEL_IF1" --ip-address="$GCP_APIPA_BGP_A" --mask-length=30 --region="$GCP_REGION"
 
 # 8. Add BGP Peer 0
 gcloud compute routers add-bgp-peer "$ROUTER_NAME" --peer-name="$BGP_PEER_IF0" --peer-asn="$AZURE_ASN_B" --interface="$INTERFACE0" --peer-ip-address="$AZURE_APIPA_BGP_A" --region="$GCP_REGION" --advertised-route-priority="$PRIORITY"
@@ -57,6 +67,7 @@ gcloud compute routers add-bgp-peer "$ROUTER_NAME" --peer-name="$BGP_PEER_IF1" -
 # These must be executed in the reverse dependency order to avoid errors.
 # =========================================================================
 
+### When removing an old connection - start here
 # 1. Remove BGP Peer 0
 gcloud compute routers remove-bgp-peer "$ROUTER_NAME" --project="$PROJECT" --region="$GCP_REGION" --peer-name="$BGP_PEER_IF0" --quiet
 
@@ -75,6 +86,7 @@ gcloud compute vpn-tunnels delete "$TUNNEL_IF0" --region="$GCP_REGION" --quiet
 # 6. Delete VPN Tunnel 1
 gcloud compute vpn-tunnels delete "$TUNNEL_IF1" --region="$GCP_REGION" --quiet
 
+### When removing an old connection - stop here
 # 7. Delete Cloud Router
 gcloud compute routers delete "$ROUTER_NAME" --region="$GCP_REGION" --quiet
 
